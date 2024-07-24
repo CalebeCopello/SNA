1898306b7326046918602efb2d2bcdf0b1fc8ae0
Calebe Copello <calebecopello@gmail.com>
2024-07-24 15:46:49 -0300

    google button added

--- frontend/src/pages/login/Login.tsx
+++ frontend/src/pages/login/Login.tsx
@@ -6,6 +6,7 @@ import { Input } from '@/components/ui/input';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
+import { FaGoogle } from 'react-icons/fa';
 
 import { styleH1, styleFormBorder, styleFormItem, styleFormLabel, styleFormMessage, styleInput, styleButton } from '../../constants/styles';
 
@@ -32,7 +33,8 @@ const Login = () => {
 	}
 
 	useEffect(() => {
-		setTimeout(() => setPageLoading(false), 3300);
+		// setTimeout(() => setPageLoading(false), 1200);
+		setPageLoading(false);
 	}, []);
 
 	console.log(pageLoading);
@@ -40,65 +42,81 @@ const Login = () => {
 	return (
 		<>
 			<div className='h-screen flex items-center justify-center'>
-				{pageLoading ? (<Skeleton className="w-[300px] h-[285px] rounded bg-color08" />) : (
-					
-				<div className={`flex w-[300px] ${styleFormBorder}`}>
-					<div className='flex-row w-full'>
-						<h1 className={`text-center ${styleH1}`}>LogIn</h1>
-						<div className=''>
-							<Form {...form}>
-								<form
-									onSubmit={form.handleSubmit(onSubmit)}
-									className='flex-row'
-								>
-									<FormField
-										control={form.control}
-										name='email'
-										render={({ field }) => (
-											<FormItem className={`${styleFormItem}`}>
-												<FormLabel className={`${styleFormLabel}`}>Email:</FormLabel>
-												<FormControl>
-													<Input
-														placeholder='Email'
-														className={`${styleInput}`}
-														{...field}
-													/>
-												</FormControl>
-												<FormMessage className={`${styleFormMessage}`} />
-											</FormItem>
-										)}
-									/>
-									<FormField
-										control={form.control}
-										name='password'
-										render={({ field }) => (
-											<FormItem className={`${styleFormItem}`}>
-												<FormLabel className={`${styleFormLabel}`}>Password:</FormLabel>
-												<FormControl>
-													<Input
-														placeholder='Password'
-														className={`${styleInput}`}
-														type='password'
-														{...field}
-													/>
-												</FormControl>
-												<FormMessage className={`${styleFormMessage}`} />
-											</FormItem>
-										)}
-									/>
-									<div className='flex flex-col items-center'>
-										<Button
-											className={`${styleButton} mt-3`}
-											type='submit'
-										>
-											Submit
-										</Button>
-									</div>
-								</form>
-							</Form>
+				{pageLoading ? (
+					<Skeleton className='w-[300px] h-[285px] rounded bg-color08' />
+				) : (
+					<div className={`flex w-[300px] ${styleFormBorder}`}>
+						<div className='flex-row w-full'>
+							<h1 className={`text-center ${styleH1}`}>LogIn</h1>
+							<div className=''>
+								<Form {...form}>
+									<form
+										onSubmit={form.handleSubmit(onSubmit)}
+										className='flex-row'
+									>
+										<FormField
+											control={form.control}
+											name='email'
+											render={({ field }) => (
+												<FormItem className={`${styleFormItem}`}>
+													<FormLabel className={`${styleFormLabel}`}>Email:</FormLabel>
+													<FormControl>
+														<Input
+															placeholder='Email'
+															className={`${styleInput}`}
+															{...field}
+														/>
+													</FormControl>
+													<FormMessage className={`${styleFormMessage}`} />
+												</FormItem>
+											)}
+										/>
+										<FormField
+											control={form.control}
+											name='password'
+											render={({ field }) => (
+												<FormItem className={`${styleFormItem}`}>
+													<FormLabel className={`${styleFormLabel}`}>Password:</FormLabel>
+													<FormControl>
+														<Input
+															placeholder='Password'
+															className={`${styleInput}`}
+															type='password'
+															{...field}
+														/>
+													</FormControl>
+													<FormMessage className={`${styleFormMessage}`} />
+												</FormItem>
+											)}
+										/>
+										<div className='flex flex-col items-center'>
+											<Button
+												className={`${styleButton} mt-3`}
+												type='submit'
+											>
+												Submit
+											</Button>
+										</div>
+									</form>
+								</Form>
+								<div className="mt-3 mx-2 flex">
+									<div className="self-center border-textColor/50 border-t w-full"></div>
+									<span className="mx-2 text-textColor flex-grow-1">
+									or
+									</span>
+									<div className="self-center border-textColor/50 border-t w-full"></div>
+								</div>
+								<div className='mt-3 flex'>
+									<Button className='mx-auto border rounded'>
+										<FaGoogle className='text-textColor' />
+										<span className="ml-2 text-textColor">
+										Google
+										</span>
+									</Button>
+								</div>
+							</div>
 						</div>
 					</div>
-				</div>
 				)}
 			</div>
 		</>
