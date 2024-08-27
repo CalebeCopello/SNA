851495b609f81e6b58e10831f505b5befd4a4004
Calebe Copello <calebecopello@gmail.com>
2024-08-27 20:32:14 -0300

    add persist state for theme reducer

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,12 +1,28 @@
 import { configureStore } from '@reduxjs/toolkit';
-import themeReducer from '../features/theme/themeSlice'
+import themeReducer from '../features/theme/themeSlice';
+import storage from 'redux-persist/lib/storage';
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+
+const persistConfig = {
+	key: 'root',
+	version: 1,
+	storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, themeReducer);
 
 export const store = configureStore({
 	reducer: {
-		theme: themeReducer,
-		// user: userReducer,
+		theme: persistedReducer,
 	},
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+			},
+		}),
 });
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export const persistor = persistStore(store);
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
